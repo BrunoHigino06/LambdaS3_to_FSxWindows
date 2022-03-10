ed54e09488de9f22e6dc2ca198c85e54706c87d5
Bruno Higino de Souza <brunohigino06@gmail.com>
2022-03-10 12:02:08 -0300

    20220310

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -1,15 +1,6 @@
 import boto3
-import os
-import shutil
 
-s3 = boto3.resource('s3')
+datasync = boto3.client('datasync', region_name='YOUR_REGION')
 
 def lambda_handler(event, context):
-    source = os.getenv('source')
-    file_name = event['Records'][0]['s3']['object']['key']
-    s3.meta.client.download_file(source, file_name, f'/tmp/{file_name}')
-
-    original = fr'/tmp/{file_name}'
-    target = fr'\\\\ec2-44-203-144-203.compute-1.amazonaws.com\\C:\\{file_name}'
-
-    shutil.copyfile(original, target)
+    datasync.start_task_execution(TaskArn='arn:aws:datasync:YOUR_REGION:YOUR_USER_ID:task/YOUR_TASK_ID')
